13da08026ed73fb135342ab15b38c161c7ff7fb3
priya-002 <priya.md0002@gmail.com>
2021-05-27 19:41:09 +0530

    pages

--- src/partials/Features.js
+++ src/partials/Features.js
@@ -7,6 +7,8 @@ import"./table.css"
 function Features() {
 
   const [tab, setTab] = useState(1);
+  
+  const[isSubmitted,setisSubmitted]=useState(false)
 
   const tabs = useRef(null);
 
@@ -38,6 +40,7 @@ function Features() {
      
      const interest = (x * (y * 0.01))/z;
       setFinal(interest)
+      setisSubmitted(true)
       
       console.log(final)
 
@@ -159,11 +162,7 @@ function Features() {
                 <input id="check" type="checkbox" className="check" checked />
                 <label for="check"><span className="icon"></span> I agree to terms and policies</label>
             </div>
-            <div className="group">
-              
-                <input type="submit" className="button" value="Submit" />
-              
-            </div>
+           
             
             <div className="hr"></div>
            
@@ -219,7 +218,32 @@ function Features() {
                
                  
                   <div className="relative inline-flex flex-col">
-                 <img className="md:max-w-none mx-auto rounded" src={require('../images/farmer.jpg').default} width="500" height="462" alt="Features bg" />
+                    {!isSubmitted? <img className="md:max-w-none mx-auto rounded" src={require('../images/farmer.jpg').default} width="500" height="462" alt="Features bg" />:<div> <div style={{marginTop:"0%"}} className="max-w-3xl mx-auto mt-20" data-aos="zoom-y-out">
+      <div className="relative flex items-start border-2 border-gray-200 rounded bg-white">
+
+        {/* Testimonial */}
+        <div className="text-center px-12 py-8 pt-20 mx-4 md:mx-0">
+          <div className="absolute top-0 -mt-8 left-1/2 transform -translate-x-1/2">
+            <svg className="absolute top-0 right-0 -mt-3 -mr-8 w-16 h-16 fill-current text-blue-500" viewBox="0 0 64 64" aria-hidden="true" xmlns="http://www.w3.org/2000/svg">
+              <path d="M37.89 58.338c-2.648-5.63-3.572-10.045-2.774-13.249.8-3.203 8.711-13.383 23.737-30.538l2.135.532c-6.552 10.033-10.532 17.87-11.939 23.515-.583 2.34.22 6.158 2.41 11.457l-13.57 8.283zm-26.963-6.56c-2.648-5.63-3.572-10.046-2.773-13.25.799-3.203 8.71-13.382 23.736-30.538l2.136.533c-6.552 10.032-10.532 17.87-11.94 23.515-.583 2.339.22 6.158 2.41 11.456l-13.57 8.283z" />
+            </svg>
+            
+          </div>
+          <blockquote className="text-xl font-medium mb-4">
+          <div className="text-gray-600">
+          
+</div>
+          </blockquote>
+          <cite className="block font-bold text-lg not-italic mb-1"></cite>
+          <div className="text-gray-600">
+          Your respected interest rate is{final} Rupees.<br/>  Please fill in all details for us to hear you.Thank you for trust in us .<br/>
+            <span></span> <a className="text-blue-600 hover:underline" href="#0"></a>
+          </div>
+        </div>
+       
+      </div>
+    </div></div> }
+                
                     
                    
                   </div>
